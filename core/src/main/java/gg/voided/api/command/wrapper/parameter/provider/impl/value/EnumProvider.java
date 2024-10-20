d7ebbe386a54205b83dc9179e310ffb7cc604c9f
J4C0B3Y <J4C0B3Y@gmail.com>
2024-10-20 15:38:52 +1100

    start enum provider

---
+++ core/src/main/java/gg/voided/api/command/wrapper/parameter/provider/impl/value/EnumProvider.java
@@ -0,0 +1,10 @@
+package gg.voided.api.command.wrapper.parameter.provider.impl.value;
+
+/**
+ * @author J4C0B3Y
+ * @version CommandAPI
+ * @since 20/10/2024
+ */
+// TODO: Enum Provider
+public class EnumProvider {
+}
