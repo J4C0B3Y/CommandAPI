934cbafd0999f0c4eab2a997d0b6c7d50ea0fed1
J4C0B3Y <J4C0B3Y@gmail.com>
2024-10-27 02:24:52 +1100

    don't register listener if plugin is disabled

--- bukkit/src/main/java/net/j4c0b3y/api/command/bukkit/BukkitCommandHandler.java
+++ bukkit/src/main/java/net/j4c0b3y/api/command/bukkit/BukkitCommandHandler.java
@@ -48,15 +48,17 @@ public class BukkitCommandHandler extends CommandHandler {
 
         setTranslator(text -> ChatColor.translateAlternateColorCodes('&', text));
 
-        Bukkit.getScheduler().runTask(plugin, () ->
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            if (!plugin.isEnabled()) return;
+
             ClassUtils.ifPresent("com.destroystokyo.paper.event.server.AsyncTabCompleteEvent", () -> {
                 plugin.getServer().getPluginManager().registerEvents(new AsyncTabListener(this), plugin);
 
                 if (isDebug()) {
                     plugin.getLogger().info("Enabled async tab completion support.");
                 }
-            })
-        );
+            });
+        });
     }
 
     @Override
